2c7ad6e2bec1d3529cd2ef42f7f87d5064468c38
Tarang Parmar <dev5.apolloinfotech@gmail.com>
2026-07-11 19:28:09 +0530

    updated Code

--- app/Mail/WelcomeMail.php
+++ app/Mail/WelcomeMail.php
@@ -10,18 +10,22 @@ class WelcomeMail extends Mailable
 {
     use Queueable, SerializesModels;
 
-    public $user;
+    public object $user;
     public $plainPassword;
 
     public function __construct($user, ?string $plainPassword = null)
     {
-        $this->user = $user;
+         $this->user = (object) [
+            'name' => $user->name,
+            'email' => $user->email,
+        ];
         $this->plainPassword = $plainPassword;
     }
 
     public function build()
     {
         return $this->subject('Your Balantro Account Setup Details')
+            ->to($this->user->email)
             ->view('emails.welcome')
             ->with([
                 'plainPassword' => $this->plainPassword,
