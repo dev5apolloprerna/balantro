4856a4e564b008e148c0d8883a1be309225a75c4
Tarang Parmar <dev5.apolloinfotech@gmail.com>
2026-08-06 12:20:09 +0530

    first commit

---
+++ storage/framework/views/992a38ee65c3ba586ec6d88e640d7bd9.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php echo $__env->make('includes.head', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+</head>
+
+<body class="antialiased font-sans selection:bg-balantro-primary selection:text-white">
+    <?php echo $__env->make('shared.form_validation', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+    <?php echo $__env->make('includes.header', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+
+    
+    <?php echo $__env->yieldContent('content'); ?>
+
+    <?php echo $__env->make('includes.footer', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+    <?php echo $__env->yieldContent('scripts'); ?>
+
+    <?php echo $__env->make('includes.footer-scripts', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+
+</body>
+
+</html>
+<?php /**PATH D:\xampp\htdocs\balantro\resources\views/layouts/front.blade.php ENDPATH**/ ?>
